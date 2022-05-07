e25e90da696b8a6193765b78de83b0f0002d5847
Gaurav <gauravchauhan3030@gmail.com>
2022-05-07 18:21:13 +0530

    matchRoutes function

--- src/index.js
+++ src/index.js
@@ -6,11 +6,16 @@ import "babel-polyfill";
 import express from "express";
 import createStore from "./helpers/createStore";
 import renderer from "./helpers/renderer";
+import { matchRoutes } from "react-router-config";
+import Routes from "./client/Routes";
 
 const app = express();
 app.use(express.static("public"));
 app.get("*", (req, res) => {
   const store = createStore();
+
+  console.log(matchRoutes(Routes, req.path));
+
   res.send(renderer(req, store));
 });
 app.listen(3000, () => {
