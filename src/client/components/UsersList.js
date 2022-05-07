4f2c4d205d1d6a540493a863144d37f6916c1d61
Gaurav <gauravchauhan3030@gmail.com>
2022-05-07 17:09:41 +0530

    UsersList component

---
+++ src/client/components/UsersList.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import connect from "react-redux/lib/connect/connect";
+import { fetchUsers } from "../actions/index";
+
+class UsersList extends Component {
+  componentDidMount() {
+    // this.props.fetchUsers()
+  }
+
+  renderUsers() {
+    return this.props.users.map((user) => {
+      return <li key={user.id}>{user.name}</li>;
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        Heres a big list
+        <ul>{this.renderUsers()}</ul>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { users: state.users };
+}
+
+export default connect(mapStateToProps, { fetchUsers })(UsersList);
