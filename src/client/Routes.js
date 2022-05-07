609ba0d94faaf1c8af0b026447dd23476af1f8ab
Gaurav <gauravchauhan3030@gmail.com>
2022-05-07 18:13:57 +0530

    react router config

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -3,11 +3,14 @@ import { Route } from "react-router-dom";
 import Home from "./components/Home";
 import UsersList from "./components/UsersList";
 
-export default () => {
-  return (
-    <div>
-      <Route exact path="/" component={Home} />
-      <Route path="/users" component={UsersList} />
-    </div>
-  );
-};
+export default [
+  {
+    path: "/",
+    exact: true,
+    component: Home,
+  },
+  {
+    path: "/users",
+    component: UsersList,
+  },
+];
