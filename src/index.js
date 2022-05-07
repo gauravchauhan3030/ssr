8b3f2ed99145523c5b62fe2ab33cca87916a7426
Gaurav <gauravchauhan3030@gmail.com>
2022-05-07 18:43:59 +0530

    waiting for data loading completion

--- src/index.js
+++ src/index.js
@@ -17,6 +17,10 @@ app.get("*", (req, res) => {
   const promises = matchRoutes(Routes, req.path).map(({ route }) => {
     return route.loadData ? route.loadData(store) : null;
   });
+
+  Promise.all(promises).then(() => {
+    res.send(renderer(req, store));
+  });
 });
 app.listen(3000, () => {
   console.log("Listening on port 3000");
