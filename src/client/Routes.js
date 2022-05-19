e9740366fbce5151a68f68cc5b812fe8e9672699
Gaurav <gauravchauhan3030@gmail.com>
2022-05-19 12:03:32 +0530

    refactoring pages and exports

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -1,17 +1,16 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import Home from "./components/Home";
-import UsersList, { loadData } from "./components/UsersList";
+import HomePage from "./pages/HomePage";
+import UsersListPage from "./pages/UsersListPage";
 
 export default [
   {
+    ...HomePage,
     path: "/",
     exact: true,
-    component: Home,
   },
   {
-    loadData: loadData,
+    ...UsersListPage,
     path: "/users",
-    component: UsersList,
   },
 ];
--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { renderToString } from "react-dom/server";
-import Home from "../client/components/Home";
 import { StaticRouter } from "react-router-dom";
 import Routes from "../client/Routes";
 import { Provider } from "react-redux";
